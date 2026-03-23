0c5ba71b9db50e405c0917d26bb533628b131739
Ultrafeeder4 <ultrafeeder4@gmail.com>
2026-03-22 23:48:37 -0400

    added nixos config

---
+++ nixos/configuration.nix
@@ -0,0 +1,217 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{  pkgs, ... }:
+let
+	home-manager = fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+	(import "${home-manager}/nixos")
+    ];
+  nix.extraOptions = ''
+  experimental-features = nix-command flakes
+  '';
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "black_tower"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = false;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ultrafeeder4 = {
+    isNormalUser = true;
+    description = "ultrafeeder4";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+	home-manager.users.ultrafeeder4 = {pkgs, ...}: {
+		home.packages = with pkgs; [
+			# utils
+			cmake
+      btop
+			wl-clipboard
+      wmenu
+			manix
+      gamescope
+      wl-restart
+      wlr-randr
+      font-manager
+      pamixer
+      fnott
+      fzf
+      nerd-fonts.jetbrains-mono
+      libgcc
+
+
+      # apps
+      orca-slicer
+      openscad
+      spotify
+      discord
+      
+      #lsps/languages
+      typescript
+      typescript-language-server
+      bash-language-server
+      vscode-langservers-extracted
+      ccls
+      openscad-lsp
+
+		];		
+	programs = {
+		    emacs = {
+      enable = true;
+      package = pkgs.emacs;
+    };
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Ultrafeeder4";
+          email = "ultrafeeder4@gmail.com";
+        };
+        init.defaultBranch = "main";
+      };
+    };
+		neovim = {
+			enable = true;
+			viAlias = true;
+      extraPackages = with pkgs; [
+        bash-language-server
+        ccls
+        vscode-langservers-extracted
+        lua-language-server
+        typescript
+        typescript-language-server
+        pyright
+      ];
+		};
+    };
+  nixpkgs.config.allowUnfree = true;
+	home.stateVersion = "25.11";
+	};
+
+  # Install firefox.
+  programs = {
+    bash.enable = true;
+    direnv = {
+			enable = true;
+			enableBashIntegration = true;
+			nix-direnv.enable = true;
+		};
+   firefox.enable = true;
+   steam.enable = true;
+};
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	vim
+  wayfire
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
